9e50745f948ff1ee954d7fbce086d6593774a754
Joap_coelhp <jpcoelho99@gmail.com>
2019-12-02 16:28:44 +0000

    viewmini

---
+++ ERecarga/ERecarga/Controllers/PromotionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERecarga.DAL;
+using ERecarga.Models;
+
+namespace ERecarga.Controllers
+{
+    public class PromotionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Promotions
+        public ActionResult Index()
+        {
+            var promotions = db.Promotions.Include(p => p.FillStation);
+            return View(promotions.ToList());
+        }
+
+        // GET: Promotions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Promotion promotion = db.Promotions.Find(id);
+            if (promotion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(promotion);
+        }
+
+        // GET: Promotions/Create
+        public ActionResult Create()
+        {
+            ViewBag.FillStationId = new SelectList(db.FillStations, "Id", "Name");
+            return View();
+        }
+
+        // POST: Promotions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FillStationId,Price,PromotionStart,PromotionEnd")] Promotion promotion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Promotions.Add(promotion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FillStationId = new SelectList(db.FillStations, "Id", "Name", promotion.FillStationId);
+            return View(promotion);
+        }
+
+        // GET: Promotions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Promotion promotion = db.Promotions.Find(id);
+            if (promotion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FillStationId = new SelectList(db.FillStations, "Id", "Name", promotion.FillStationId);
+            return View(promotion);
+        }
+
+        // POST: Promotions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FillStationId,Price,PromotionStart,PromotionEnd")] Promotion promotion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(promotion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FillStationId = new SelectList(db.FillStations, "Id", "Name", promotion.FillStationId);
+            return View(promotion);
+        }
+
+        // GET: Promotions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Promotion promotion = db.Promotions.Find(id);
+            if (promotion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(promotion);
+        }
+
+        // POST: Promotions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Promotion promotion = db.Promotions.Find(id);
+            db.Promotions.Remove(promotion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
