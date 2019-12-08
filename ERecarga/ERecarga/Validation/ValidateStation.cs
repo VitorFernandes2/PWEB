cbecb73e1b92c54b8b3788d73d3ea18014fd4dca
isJoaoCoelho <55954421+isJoaoCoelho@users.noreply.github.com>
2019-12-08 16:50:43 +0000

    outro micro commit

--- ERecarga/ERecarga/Validation/ValidateStation.cs
+++ ERecarga/ERecarga/Validation/ValidateStation.cs
@@ -7,5 +7,23 @@ namespace ERecarga.Validation
 {
     public class ValidateStation
     {
+        public static bool AlreadyExistsStation(Station station)
+        {
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            foreach (var item in db.Stations.ToList())
+            {
+
+                if (item.Name == station.Name || item.Id == station.Id)
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+
+        }
     }
 }
--- ERecarga/ERecarga/Validation/ValidateTimeBreak.cs
+++ ERecarga/ERecarga/Validation/ValidateTimeBreak.cs
@@ -7,5 +7,24 @@ namespace ERecarga.Validation
 {
     public class ValidateTimeBreak
     {
+        public static bool AlreadyExistsTimeBreak(FillStationTimeBreak fillStationTimeBreak)
+        {
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            foreach (var item in db.FillStationTimeBreaks.ToList())
+            {
+
+                if (item.FillStationId == fillStationTimeBreak.FillStationId && item.TimeBreakId == fillStationTimeBreak.TimeBreakId)
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+
+        }
+
     }
 }
