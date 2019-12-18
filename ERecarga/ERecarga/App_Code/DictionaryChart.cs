f4984a2b0f9f89008cdd99277bcfed77c5d92208
VitorFernandes2 <vhferprog@gmail.com>
2019-12-18 14:35:25 +0000

    Chart acabado

---
+++ ERecarga/ERecarga/App_Code/DictionaryChart.cs
@@ -0,0 +1,53 @@
+﻿using ERecarga.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERecarga.App_Code
+{
+    public class DictionaryChart
+    {
+
+        public static Dictionary<string, int> createListItems(ApplicationDbContext db, string userId)
+        {
+
+            Dictionary<string, int> dictionaryChart = new Dictionary<string, int>();
+
+            var reservations = db.Reservations.ToList();
+
+            foreach(var item in reservations.ToList())
+            {
+
+                if(item.FillStationTimeBreak.FillStation.Station.OwnerId.Equals(userId))
+                {
+
+                    if (dictionaryChart.ContainsKey(item.FillStationTimeBreak.FillStation.Name))
+                    {
+
+                        foreach (var item2 in dictionaryChart)
+                        {
+                            if (item2.Key.Equals(item.FillStationTimeBreak.FillStation.Name))
+                            {
+                                dictionaryChart[item2.Key] = item2.Value + 1;
+                                break;
+                            }                                
+                        }
+
+                    } else
+                    {
+
+                        dictionaryChart.Add(item.FillStationTimeBreak.FillStation.Name, 1);
+
+                    }
+
+                }
+
+            }
+
+            return dictionaryChart;
+
+        }
+
+    }
+}
--- ERecarga/ERecarga/Controllers/StatisticsController.cs
+++ ERecarga/ERecarga/Controllers/StatisticsController.cs
@@ -1,4 +1,6 @@
-﻿using ERecarga.Models;
+﻿using ERecarga.App_Code;
+using ERecarga.Models;
+using Microsoft.AspNet.Identity;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -7,17 +9,29 @@ namespace ERecarga.Controllers
 {
     public class StatisticsController : Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Statistics
+        [Authorize(Roles = "Owner")]
         public ActionResult Index()
         {
             List<DataPoint> dataPoints = new List<DataPoint>();
 
-            dataPoints.Add(new DataPoint("Samsung", 25));
-            dataPoints.Add(new DataPoint("Micromax", 13));
-            dataPoints.Add(new DataPoint("Lenovo", 8));
-            dataPoints.Add(new DataPoint("Intex", 7));
-            dataPoints.Add(new DataPoint("Reliance", 6.8));
-            dataPoints.Add(new DataPoint("Others", 40.2));
+            string userid = User.Identity.GetUserId();
+            Dictionary<string, int> dict = DictionaryChart.createListItems(db, userid);
+
+            foreach(var item in dict)
+            {
+                dataPoints.Add(new DataPoint(item.Key, dict[item.Key]));
+            }
+
+            //dataPoints.Add(new DataPoint("Samsung", 25));
+            //dataPoints.Add(new DataPoint("Micromax", 13));
+            //dataPoints.Add(new DataPoint("Lenovo", 8));
+            //dataPoints.Add(new DataPoint("Intex", 7));
+            //dataPoints.Add(new DataPoint("Reliance", 6.8));
+            //dataPoints.Add(new DataPoint("Others", 40.2));
 
             ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
 
